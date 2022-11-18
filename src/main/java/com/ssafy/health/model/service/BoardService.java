ae49f1d5f2b6be78d91cc2d4ec537178b391282e
jay <anastia0609@gmail.com>
2022-11-18 11:28:24 +0900

    added board to spring

---
+++ src/main/java/com/ssafy/health/model/service/BoardService.java
@@ -0,0 +1,12 @@
+package com.ssafy.health.model.service;
+
+
+import com.ssafy.health.model.dto.Board;
+
+import java.util.List;
+
+public interface BoardService {
+    List<Board> getBoardList();
+
+
+}
